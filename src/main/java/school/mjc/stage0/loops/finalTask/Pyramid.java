773f9a6dd144e723017640442efb8e5c0e5a7117
Armine8 <armisha15@mail.ru>
2022-11-09 20:53:46 +0300

    Module #4.2 Loops. Final task

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {  //первый столбик
+            for (int j = 1; j < cathetusLength+1-i; j++) { // пробелы до цифр
+                System.out.print(" ");
+            }
+            for (int num = i; num >= 1; num--){ //цифры до 1
+                System.out.print(num);
+            }
+            for (int k = 2; k <= i; k++){ //от 1
+                System.out.print(k);
+            }
+        System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(9);
     }
 }
